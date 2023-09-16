6028769d65faa5a2f26e429bd2f8d47462e165c8
Tynara <parshath@web.de>
2023-09-16 16:29:58 +0200

    WIP

---
+++ Catherding/Assets/_scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    new public Rigidbody rigidbody;
+
+    [SerializeField] float speed = 100;
+    [SerializeField] float vertical_speed = 100;
+    [SerializeField] float height = 5;
+
+    [SerializeField] string horizontal_axis = "Horizontal1";
+    [SerializeField] string vertical_axis = "Vertical1";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = new Vector3(Input.GetAxis(horizontal_axis), 0, Input.GetAxis(vertical_axis));
+        direction *= speed * Time.deltaTime;
+
+        RaycastHit hit; 
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit))
+        {
+            float h = hit.point.y + height;
+            direction.y = h - transform.position.y;
+            direction.y *= vertical_speed * Time.deltaTime;
+        }
+
+        Vector3 pos = transform.position + direction;
+        pos.y = Mathf.Max(pos.y, 3);
+
+        rigidbody.MovePosition(pos);
+    }
+}
